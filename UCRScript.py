55d65186c7ef88fb50783a72349d158fd7200530
WinterChilly <pesjak@users.noreply.github.com>
2019-09-17 12:45:45 +0200

    Initial commit

---
+++ UCRScript.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+
+def correctNameFile(file, dirs):
+    if file.endswith(("mkv", "mp4", "mkv", "flv", "wmv", "avi", "mpg", "mpeg", "mp3", "avi")):
+        name, extension = os.path.splitext(file)
+        result = re.sub("[\(\[].*?[\)\]]", "", name).strip()
+        episode_number = [int(s) for s in result.split() if s.isdigit()]
+        if len(episode_number) == 1:
+            episode_number = episode_number[0]
+        name = ' '.join(i for i in result.split() if i.isalpha())
+        result = "{} - {}{}".format(name, episode_number, extension)
+        print(result)
+        # os.rename(file, result)
+        os.rename(os.path.join(dirs, file), os.path.join(dirs, result))
+
+path = "."
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        correctNameFile(file, subdir)
