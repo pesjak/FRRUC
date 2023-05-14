fdd144cf675efd18e874edca13abdc580bdf69c7
Primož Pesjak <pesjak@users.noreply.github.com>
2023-05-14 20:44:45 +0200

    Update FRRUCScript.py Final FIX

--- FRRUCScript.py
+++ FRRUCScript.py
@@ -3,7 +3,7 @@ import re
 
 def remove_unnecessary_characters(file_name):
     # Remove square brackets and their contents
-    file_name = re.sub(r'\[[^\]]*\]', '', file_name)
+    file_name = re.sub(r'\[[^]]*\]', '', file_name)
     
     # Remove parentheses and their contents
     file_name = re.sub(r'\([^)]*\)', '', file_name)
@@ -14,8 +14,8 @@ def remove_unnecessary_characters(file_name):
     return file_name
 
 def extract_episode_number(file_name):
-    # Find the last occurrence of a sequence of digits followed by optional whitespace
-    episode_number = re.search(r'(\d+)\s*$', file_name[::-1])
+    # Find the last occurrence of a sequence of digits
+    episode_number = re.search(r'\b(\d+)\b', file_name[::-1])
     if episode_number:
         episode_number = episode_number.group(1)
         # Reverse the episode number back to the correct order
@@ -34,11 +34,23 @@ def correct_file_name(file_path):
         episode_number = extract_episode_number(new_file_name)
         
         if episode_number:
-            # Remove the episode number from the file name
-            new_file_name = re.sub(r'\d+\s*$', '', new_file_name).strip()
+            # Split the file name into components
+            components = new_file_name.split('-')
             
-            # Construct the new file name with the episode number and extension
-            new_file_name = f"{new_file_name} - {episode_number}{file_extension}"
+            # Remove leading and trailing spaces from each component
+            components = [component.strip() for component in components]
+            
+            # Remove empty components
+            components = [component for component in components if component]
+            
+            # Check if the episode number is already present in the file name
+            if episode_number not in components[-1]:
+                # Add the episode number to the last component
+                components[-1] = f"{components[-1]} - {episode_number}"
+            
+            # Reconstruct the new file name
+            new_file_name = ' - '.join(components) + file_extension
+            
             return new_file_name
     
     return None
@@ -47,7 +59,7 @@ path = "."
 for file_name in os.listdir(path):
     file_path = os.path.join(path, file_name)
     new_file_name = correct_file_name(file_path)
-    if new_file_name:
+    if new_file_name and new_file_name != file_name:
         new_file_path = os.path.join(path, new_file_name)
         os.rename(file_path, new_file_path)
         print(f"Renamed: {file_name} => {new_file_name}")
